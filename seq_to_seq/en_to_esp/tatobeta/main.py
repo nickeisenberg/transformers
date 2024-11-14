8ad6c7aaa3f88211c5b56fd5b6f8224aaf013fc1
nickeisenberg <nickeisenberg@gmail.com>
2024-11-14 11:25:49 -0800

    transformer trains. I need a bigger gpu to train longer

--- seq_to_seq/en_to_esp/tatobeta/main.py
+++ seq_to_seq/en_to_esp/tatobeta/main.py
@@ -213,7 +213,7 @@ for epoch in range(epochs):
 
 tokenizer.decode(
     transformer.inference(
-        tokenizer.encode("I am hungry", return_tensors="pt").to(0), 
+        tokenizer.encode("where is the library", return_tensors="pt").to(0), 
         max_len=20, sos_token=65001, eos_token=0
     )[0].to("cpu")
 )
